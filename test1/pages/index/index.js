eb60a9277060add3c0b1950ac021aa705d75511a
yangying <1349365218@qq.com>
2016-11-20 19:06:00 +0800

    wx add delete  Please enter the commit message for your changes. Lines starting

--- test1/pages/index/index.js
+++ test1/pages/index/index.js
@@ -4,9 +4,52 @@ var app = getApp()
 Page({
   data: {
     motto: 'Hello World',
-    userInfo: {}
+    userInfo: {},
+    newItemName: '',
+    items:[
+      {value: '美国'},
+      {value: '中国', checked: 'true'},
+      {value: '巴西'},
+      {value: '日本'},
+      {value: '英国'},
+      {value: '法国'},
+    ]
   },
   //事件处理函数
+  checkboxChange: function(e){
+    console.log('点击：' + e.detail.value);
+  },
+  getItemIndex: function(name){
+    this.data.items.filter(function(elem,index){
+      elem.value == name;
+      return index;
+    })
+  },
+  //input
+  inputNewItem: function(e){
+
+    this.setData( {
+      newItemName: e.detail.value
+    });
+  },
+  //add
+  addItem: function(){
+    console.log('add :' + this.data.newItemName);
+    if(this.data.newItemName != ''){
+      this.data.items = [{value: this.data.newItemName}].concat(this.data.items)
+      this.setData({
+        items: this.data.items
+      })
+    }
+    
+  },
+  //delete-one
+  deleteItem: function(e){
+    console.log('delete :' + e.currentTarget.id);
+    var cur_index = this.getItemIndex(e.currentTarget.id);
+    this.data.items.splice(cur_index, 1);
+
+  },
   bindViewTap: function() {
     wx.navigateTo({
       url: '../logs/logs'
